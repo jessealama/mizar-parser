445722c74b38fd8668d07bf1cf0d6ea4d2075706
Jesse Alama <jesse.alama@gmail.com>
2012-02-15 15:42:37 +0000

    A simple article class, on objects of which one can do some sime operations.

--- mizar.lisp
+++ mizar.lisp
@@ -1,28 +1,96 @@
 
 (in-package :mizar-parser)
 
-(defun run-mizar-tool (tool flags path)
+(defclass article ()
+  ((name
+    :initarg :name
+    :type string
+    :reader name
+    :documentation "The name of the article (a string of alphanumeric characters of length between 1 and 8.")
+   (directory
+    :initarg :directory
+    :type pathname
+    :accessor article-directory
+    :documentation "The directory where the .miz for this article can be found.")
+   (text
+    :initarg :text
+    :type (or nil string)
+    :reader article-text
+    :documentation "The (full) text of the article.")))
+
+(defmethod initialize-instance :after ((article article) &rest initargs &key &allow-other-keys)
+  (declare (ignore initargs))
+  (when (not (slot-boundp article 'directory))
+    (error "One must specify a directory for an article."))
+  (when (not (slot-boundp article 'name))
+    (error "One must specify a name for an article."))
+  (let ((path (miz-file article)))
+    (when (slot-boundp article 'text)
+      (write-string-into-file (slot-value article 'text)
+			      path
+			      :if-exists :supersede
+			      :if-does-not-exist :create
+			      :external-format :ascii))))
+
+(defgeneric file-with-extension (article extension))
+
+(defmethod file-with-extension ((article article) extension)
+  (let ((name (name article))
+	(dir (pathname-as-directory (article-directory article))))
+    (merge-pathnames (format nil "~a.~a" name extension) dir)))
+
+(defgeneric err-file (article))
+
+(defmethod err-file ((article article))
+  (file-with-extension article "err"))
+
+(defgeneric miz-file (article))
+
+(defmethod miz-file ((article article))
+  (file-with-extension article "miz"))
+
+(defgeneric empty-err-file? (article))
+
+(defmethod empty-err-file? ((article article))
+  (zerop
+   (with-open-file (file (err-file article))
+     (file-length file))))
+
+(defgeneric run-mizar-tool (tool flags article))
+
+(defmethod run-mizar-tool (tool flags (article article))
   #+ccl
-  (let ((proc (ccl:run-program tool
-		   (append flags (list (namestring path)))
-		   :wait t
-		   :input nil
-		   :output nil
-		   :error nil)))
+  (let* ((path (miz-file article))
+	 (proc (ccl:run-program tool
+				(append flags (list (namestring path)))
+				:wait t
+				:input nil
+				:output nil
+				:error nil)))
     (multiple-value-bind (status exit-code)
 	(ccl:external-process-status proc)
       (declare (ignore status))
-      (and (numberp exit-code)
-	   (zerop exit-code)))))
+      (values (and (numberp exit-code)
+		   (zerop exit-code))
+	      (when (and (numberp exit-code)
+			 (not (zerop exit-code)))
+		(or (not (file-exists-p (err-file article)))
+		    (empty-err-file? article))))))
+  #-ccl
+  (error "We don't handle your Common Lisp.  Sorry."))
 
 (defmacro run-mizar-tool-with-standard-flags (tool article)
   `(run-mizar-tool ,tool '("-q" "-s" "-l") ,article))
 
-(defun accom (article)
+(defgeneric accom (article))
+(defgeneric wsmparser (article))
+(defgeneric msmprocessor (article))
+
+(defmethod accom ((article article))
   (run-mizar-tool-with-standard-flags "accom" article))
 
-(defun wsmparser (article)
+(defmethod wsmparser ((article article))
   (run-mizar-tool-with-standard-flags "wsmparser" article))
 
-(defun msmprocessor (article)
+(defmethod msmprocessor ((article article))
   (run-mizar-tool-with-standard-flags "msmprocessor" article))

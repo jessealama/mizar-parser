3d86f6edf2426cb090d9643a812eeb078fa10e78
Jesse Alama <jesse.alama@gmail.com>
2012-03-06 09:16:28 -0700

    Temporary directories for SBCL.

--- utils.lisp
+++ utils.lisp
@@ -12,7 +12,18 @@
     (with-open-stream (mktemp-output
 		       (ccl:external-process-output-stream mktemp-process))
       (values (read-line mktemp-output))))
-  #-ccl
+  #+sbcl
+  (let ((mktemp-process (sb-ext:run-program "mktemp"
+					    '("-d")
+					    :wait t
+					    :search t
+					    :input nil
+					    :error nil
+					    :output :stream)))
+    (with-open-stream (mktemp-output
+		       (sb-ext:process-output mktemp-process))
+      (values (read-line mktemp-output))))
+  #- (or sbcl ccl)
   (error "We don't yet implement TEMPORARY-DIRECTORY for your Common Lisp."))
 
 (defun lines-of-file (path)

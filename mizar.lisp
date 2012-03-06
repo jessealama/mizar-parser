e8565ffbff327e57005fe0c5d41a2de4a6a380d4
Jesse Alama <jesse.alama@gmail.com>
2012-03-06 09:17:00 -0700

    More Mizar goodness.

--- mizar.lisp
+++ mizar.lisp
@@ -56,12 +56,56 @@
    (with-open-file (file (err-file article))
      (file-length file))))
 
+(define-constant +mizar-system-version+
+    "7.13.01"
+  :test #'string=
+  :documentation "The system version of the Mizar that we currently support.")
+
+(define-constant +mml-version+
+    "4.181.1147"
+  :test #'string=
+  :documentation "The MML version that we support.")
+
+(define-constant +mizar-version+
+    (format nil "~a-~a" +mizar-system-version+ +mml-version+)
+  :test #'string=
+  :documentation "The system number and MML version that we use, in that order, separated by a dash.")
+
+(define-constant +mizar-release-root-dir+
+    (pathname "/home/mizar-items/mizar/release/")
+  :test #'equal
+  :documentation "The directory under which we look for Mizar installations.")
+
+(defun path-for-tool (tool &optional (mizar-version +mizar-version+))
+  (let ((directory-for-version (merge-pathnames (format nil "~a/" mizar-version)
+						+mizar-release-root-dir+)))
+    (if (directory-p directory-for-version)
+	(let ((bin-subdir (merge-pathnames "bin/" directory-for-version)))
+	  (if (directory-p bin-subdir)
+	      (merge-pathnames tool bin-subdir)
+	      (error "The bin subdirectory~%~%  ~a~%~%of~%~%  ~a~%~%does not exist." bin-subdir directory-for-version)))
+	(error "The release directory ~a doest not exist." directory-for-version))))
+
+(defun mizfiles (&optional (mizar-system-version +mizar-system-version+)
+		           (mml-version +mml-version+))
+  (let ((full-system-name (format nil "~a-~a" mizar-system-version mml-version)))
+    (pathname (format nil "~a~a"
+		      (namestring +mizar-release-root-dir+)
+		      full-system-name))))
+
 (defgeneric run-mizar-tool (tool flags article))
+
+(defmethod run-mizar-tool :around (tool flags article)
+  (let ((tool-path (path-for-tool tool)))
+    (if (file-exists-p tool-path)
+	(call-next-method)
+	(error "We cannot find ~a at its expected location~%~%  ~a~%" tool tool-path))))
 
 (defmethod run-mizar-tool (tool flags (article article))
   #+ccl
   (let* ((path (miz-file article))
-	 (proc (ccl:run-program tool
+	 (tool-path (path-for-tool tool))
+	 (proc (ccl:run-program tool-path
 				(append flags (list (namestring path)))
 				:wait t
 				:input nil
@@ -76,8 +120,23 @@
 			 (not (zerop exit-code)))
 		(or (not (file-exists-p (err-file article)))
 		    (empty-err-file? article))))))
-  #-ccl
-  (error "We don't handle your Common Lisp.  Sorry.")
+  #+sbcl
+  (let* ((path (miz-file article))
+	 (err-path (file-with-extension article "err"))
+	 (tool-path (path-for-tool tool))
+	 (proc (sb-ext:run-program tool-path
+				   (append flags (list (namestring path)))
+				   :environment (list (format nil "MIZFILES=~a" (namestring (mizfiles))))
+				   :search nil
+				   :wait t
+				   :input nil
+				   :output nil
+				   :error nil)))
+    (let ((exit-code (sb-ext:process-exit-code proc)))
+      (values (and (numberp exit-code)
+		   (zerop exit-code))
+	      (and (file-exists-p err-path)
+		   (empty-err-file? article)))))
   #-(or ccl sbcl)
   (error "We don't handle your Common Lisp.  Sorry."))
 
@@ -87,6 +146,8 @@
 (defgeneric accom (article))
 (defgeneric wsmparser (article))
 (defgeneric msmprocessor (article))
+(defgeneric msplit (article))
+(defgeneric mglue (article))
 
 (defmethod accom ((article article))
   (run-mizar-tool-with-standard-flags "accom" article))
@@ -96,3 +157,10 @@
 
 (defmethod msmprocessor ((article article))
   (run-mizar-tool-with-standard-flags "msmprocessor" article))
+
+(defmethod msplit ((article article))
+  (run-mizar-tool-with-standard-flags "msplit" article))
+
+(defmethod mglue ((article article))
+  (run-mizar-tool-with-standard-flags "mglue" article))
+

de7ecc187f8cf76c9e69e79f5f2cfd893151dd8b
Jesse Alama <jesse.alama@gmail.com>
2012-03-06 09:16:47 -0700

    Use my little hunchentoot-utils package.

--- packages.lisp
+++ packages.lisp
@@ -1,4 +1,7 @@
 (in-package :cl-user)
 
 (defpackage :mizar-parser
-  (:use :cl :cl-ppcre :com.gigamonkeys.pathnames :alexandria :hunchentoot :cl-who))
+  (:use :cl :cl-ppcre :com.gigamonkeys.pathnames :alexandria :hunchentoot :cl-who :hunchentoot-utils))
+
+;; early cl-who configuration of attribute delimit character
+(setf cl-who:*attribute-quote-char* #\")

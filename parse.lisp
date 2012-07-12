5736824acc9b56ce535fe1861ce0fc437fcf8fc0
Jesse Alama <jesse.alama@gmail.com>
2012-07-12 17:27:04 +0200

    GET.

--- parse.lisp
+++ parse.lisp
@@ -187,8 +187,10 @@
 	   (call-next-method))
 	  ((string= method-name-lc "options")
 	   (call-next-method))
+	  ((string= method-name-lc "get")
+	   (call-next-method))
 	  (t
-	   (setf (header-out "Allow") "POST, HEAD, OPTIONS")
+	   (setf (header-out "Allow") "GET, POST, HEAD, OPTIONS")
 	   (return-message +http-method-not-allowed+
 			   :mime-type "text/plain"
 			   :message "We support only the GET, POST, HEAD, and OPTIONS HTTP methods.")))))
